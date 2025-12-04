e59f9e195207dec1f43e73390f13a9afa43307c5
Kronos <3195760+cod3rkane@users.noreply.github.com>
2025-12-04 12:15:23 -0500

    updates checkout class

--- src/services/checkout/checkout.class.ts
+++ src/services/checkout/checkout.class.ts
@@ -1,71 +1,120 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/service.class.html#custom-services
-import type { Id, NullableId, Params, ServiceInterface } from '@feathersjs/feathers'
-
+import type { Id, NullableId, Paginated, Params } from '@feathersjs/feathers'
 import type { Application } from '../../declarations'
-import type { Checkout, CheckoutData, CheckoutPatch, CheckoutQuery } from './checkout.schema'
+import { SalesforceCartClient, MockSalesforceCartClient } from '../salesforceCartClient/salesforce.class'
+import { BadRequest, GeneralError } from '@feathersjs/errors'
 
-export type { Checkout, CheckoutData, CheckoutPatch, CheckoutQuery }
+type Basket = any
+type SubmittedOrder = any
 
-export interface CheckoutServiceOptions {
-  app: Application
+interface CheckoutServiceOptions {
+  salesforceCartClient: SalesforceCartClient
 }
 
-export interface CheckoutParams extends Params<CheckoutQuery> {}
+export class CheckoutService {
+  private salesforceCartClient: SalesforceCartClient
 
-// This is a skeleton for a custom service class. Remove or add the methods you need here
-export class CheckoutService<
-  ServiceParams extends CheckoutParams = CheckoutParams
-> implements ServiceInterface<Checkout, CheckoutData, ServiceParams, CheckoutPatch> {
-  constructor(public options: CheckoutServiceOptions) {}
+  constructor(options: CheckoutServiceOptions) {
+    this.salesforceCartClient = options.salesforceCartClient
+  }
 
-  async find(_params?: ServiceParams): Promise<Checkout[]> {
+  // These are not standard Feathers methods, but custom ones.
+  // We will expose them via custom routes.
+  async getBasket(params: Params): Promise<Basket> {
+    const { organizationId, basketId } = params.query || {}
+
+    if (!organizationId) {
+      throw new BadRequest('organizationId is required')
+    }
+    if (!basketId) {
+      throw new BadRequest('basketId is required')
+    }
+
+    return this.salesforceCartClient.getBaskets(organizationId, basketId)
+  }
+
+  async removeItems(id: string, params: Params): Promise<Basket> {
+    const { organizationId, basketId } = params.query || {}
+    const itemId = id
+
+    if (!organizationId) {
+      throw new BadRequest('organizationId is required')
+    }
+    if (!basketId) {
+      throw new BadRequest('basketId is required')
+    }
+    if (!itemId) {
+      throw new BadRequest('itemId is required')
+    }
+
+    return this.salesforceCartClient.deleteBasketItems(organizationId, basketId, itemId)
+  }
+
+  async patchItems(id: NullableId, data: any, params?: Params): Promise<Basket> {
+    console.log({ data, params })
+    if (params) {
+      const { organizationId, basketId } = params.query || { organizationId: 'test', basketId: 'test' }
+      const itemId = id?.toString()
+      const { itemObject } = data
+
+      if (!organizationId) {
+        throw new BadRequest('organizationId is required')
+      }
+      if (!basketId) {
+        throw new BadRequest('basketId is required')
+      }
+      if (!itemId) {
+        throw new BadRequest('itemId is required')
+      }
+      if (!itemObject) {
+        throw new BadRequest('itemObject is required in the body')
+      }
+
+      return this.salesforceCartClient.patchBasketItems(organizationId, basketId, itemId, itemObject)
+
+    }
+
+  }
+
+  async placeOrder(data: {}, params: Params): Promise<SubmittedOrder> {
+    const { organizationId, basketId } = params.query || {}
+
+    if (!organizationId) {
+      throw new BadRequest('organizationId is required')
+    }
+    if (!basketId) {
+      throw new BadRequest('basketId is required')
+    }
+
+    return this.salesforceCartClient.placeOrder(organizationId, basketId)
+  }
+
+  // Standard Feathers methods - we need to implement them, but they will not be used
+  async find(params?: Params): Promise<any> {
     return []
   }
 
-  async get(id: Id, _params?: ServiceParams): Promise<Checkout> {
-    return {
-      id: 0,
-      text: `A new message with ID: ${id}!`
-    }
+  async get(id: Id, params?: Params): Promise<any> {
+    return {}
   }
 
-  async create(data: CheckoutData, params?: ServiceParams): Promise<Checkout>
-  async create(data: CheckoutData[], params?: ServiceParams): Promise<Checkout[]>
-  async create(data: CheckoutData | CheckoutData[], params?: ServiceParams): Promise<Checkout | Checkout[]> {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)))
-    }
-
-    return {
-      id: 0,
-      ...data
-    }
+  async create(data: any, params?: Params): Promise<any> {
+    return {}
   }
 
-  // This method has to be added to the 'methods' option to make it available to clients
-  async update(id: NullableId, data: CheckoutData, _params?: ServiceParams): Promise<Checkout> {
-    return {
-      id: 0,
-      ...data
-    }
+  async update(id: NullableId, data: any, params?: Params): Promise<any> {
+    return {}
   }
 
-  async patch(id: NullableId, data: CheckoutPatch, _params?: ServiceParams): Promise<Checkout> {
-    return {
-      id: 0,
-      text: `Fallback for ${id}`,
-      ...data
-    }
+  async patch(id: NullableId, data: any, params?: Params): Promise<any> {
+    return {}
   }
 
-  async remove(id: NullableId, _params?: ServiceParams): Promise<Checkout> {
-    return {
-      id: 0,
-      text: 'removed'
-    }
+  async remove(id: NullableId, params?: Params): Promise<any> {
+    return {}
   }
 }
 
 export const getOptions = (app: Application) => {
-  return { app }
+  const salesforceCartClient = new MockSalesforceCartClient();
+  return { salesforceCartClient }
 }
